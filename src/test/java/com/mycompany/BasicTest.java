b781763e8aa9c2fdcc3394354aa0a15036d77878
Balaji <balajipusuluri26@gmail.com>
2018-06-20 19:29:30 +0530

    Basic

---
+++ src/test/java/com/mycompany/BasicTest.java
@@ -0,0 +1,28 @@
+package com.mycompany;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class BasicTest {
+  @Test(dataProvider = "dp")
+  public void basicTest(Integer n, String s) {
+  }
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
